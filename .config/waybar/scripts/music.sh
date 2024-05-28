d913ea56d9bcd937a4ec8111fd99a664a21605e0
Matheus <leirommatheus@protonmail.com>
2024-05-28 08:27:46 -0300

    add/edit music related stuff

--- .config/waybar/scripts/music.sh
+++ .config/waybar/scripts/music.sh
@@ -2,14 +2,12 @@
 
 #list of ignored players sepparated by spaces
 IP="firefox"
-player_status=$(playerctl status -i $IP)
+player_status=$(playerctl status -i $IP 2>/dev/null)
 if [[ $player_status = "Paused" ]] then
 	icon="󰏤"
 else
 	icon="󰎇"
 fi
-artist=$(playerctl -i $IP metadata artist)
-title=$(playerctl -i $IP metadata title)
-text="$artist - $title"
-echo "$icon $text"
+
+echo "$(playerctl metadata --format "$icon {{artist}} - {{title}}" 2>/dev/null)"
 
